d28dac92cf4c8632288f801c6aecfb9ad1b8ce09
Ellis Percival <flyte@failcode.co.uk>
2017-03-06 16:29:28 +0000

    Comment out unused image code.

--- avtoolkit/image.py
+++ avtoolkit/image.py
@@ -1,10 +1,10 @@
-import os
+# import os
 
-from PIL import Image as PILImage
+# from PIL import Image as PILImage
 
 
-class Image(object):
-    def __init__(self, source):
-        if not os.path.exists(source):
-            raise IOError("File does not exist at %r" % source)
-        self.source = source
+# class Image(object):
+#     def __init__(self, source):
+#         if not os.path.exists(source):
+#             raise IOError("File does not exist at %r" % source)
+#         self.source = source
